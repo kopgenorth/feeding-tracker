ae53268909bb3e461a33030ea3dafb3a092279cc
Kory Opgenorth <kiaser90@gmail.com>
2015-11-04 16:47:56 -0600

    added feeding service tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -38,26 +38,93 @@ describe("The feeding tracker app", function () {
         })
 
         it("should return a new feeding object with the time set", function () {
-//            chai.assert.equal($scope.feedingEntry, {});
+            //            chai.assert.equal($scope.feedingEntry, {});
             $scope.getNewFeeding();
             chai.assert.typeOf($scope.feedingEntry.time, 'date');
         })
 
-        /*it("should sum to the amount of each feeding", function () {
-            $scope.addFeeding({
+
+    })
+
+    describe("the feeding service", function () {
+        beforeEach(function () {
+            module('FeedingTracker');
+            inject(function ($injector) {
+                feedingService = $injector.get("FeedingService");
+            })
+        })
+
+        it("should start with an empty feedings array", function () {
+            chai.assert.equal(feedingService.feedings.length, 0);
+        })
+
+        it("should increase feeding array when calling addFeeding()", function () {
+            feedingService.addFeeding({
+                person: 'kory',
+                time: new Date(),
+                type: 'Bottle',
+                meds: 1,
+                amount: 90
+            });
+            chai.assert.equal(feedingService.feedings.length, 1);
+        })
+
+        it("should sum to the amount of each feeding", function () {
+            feedingService.addFeeding({
                 amount: 27
             });
-            $scope.addFeeding({
+            feedingService.addFeeding({
                 amount: 34
             });
-            $scope.addFeeding({
+            feedingService.addFeeding({
                 amount: 21
             });
-            $scope.addFeeding({
+            feedingService.addFeeding({
                 amount: 86
             });
 
-            chai.assert.equal($scope.totalAmount, 168)
-        })*/
+            chai.assert.equal(feedingService.getTotalAmount(), 168)
+        })
+
+        it("should average the hour interval between feedings", function () {
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 5, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 6, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 8, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 10, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 13, 0, 0,0)
+            });
+
+            chai.assert.equal(feedingService.getAverageInterval(), 2)
+        })
+
+        it("should average the hour interval between feedings - regardless of order entered", function () {
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 5, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 13, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 6, 0, 0,0)
+            });
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 10, 0, 0,0)
+            });
+
+            feedingService.addFeeding({
+                time: new Date(2015, 11, 1, 8, 0, 0,0)
+            });
+
+            chai.assert.equal(feedingService.getAverageInterval(), 2)
+        })
     })
 })
